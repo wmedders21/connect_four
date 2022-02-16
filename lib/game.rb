7aba400ea0f8bbf92c25f97102d32bba4d3ca77b
Will Medders <wmedders@gmail.com>
2022-02-16 09:50:07 -0500

    Feat: two player option

--- lib/game.rb
+++ lib/game.rb
@@ -14,7 +14,7 @@ class Game
 
   def start_menu
     puts "WELCOME TO CONNECT FOUR!"
-    puts "Please press p to play. Or, enter q to quit."
+    puts "Please enter p to play. Or, enter q to quit."
     start_menu_input = gets.chomp.upcase.strip
       until start_menu_input.include?("P") == true || start_menu_input.include?("Q") == true
         puts "Please try again."
@@ -22,9 +22,25 @@ class Game
       end
       case start_menu_input
       when "P"
-        start
+        play_mode
       when "Q"
         exit!
+      end
+    end
+
+  def play_mode
+    puts "Enter 1 for single-player mode."
+    puts "Enter 2 for 2-player mode."
+    mode_input = gets.chomp.upcase.strip
+      until mode_input.include?("1") == true || mode_input.include?("2") == true
+        puts "Please try again."
+        mode_input = gets.chomp.upcase.strip
+      end
+      case mode_input
+      when "1"
+        start
+      when "2"
+        start_2
       end
     end
 
@@ -70,6 +86,71 @@ class Game
         end
       end
       @board.add_o(@bot_turn.low_point,@bot_turn.column_address[@bot_turn.column_choice])
+      @board.render
+      @board.win_scan
+      @board.draw?
+    end
+    game_2 = Game.new
+    game_2.start_menu
+  end
+
+  def start_2
+    @board.render
+    until @board.win_scan == true || @board.draw? == true
+      puts "Player 1 turn"
+      @user_turn.go
+      if @user_turn.valid_input? == false
+        puts "Please make a valid selection A - G"
+        until @user_turn.valid_input? == true
+          @user_turn.go
+        end
+      end
+      @user_turn.process_input(@user_turn.column_choice)
+      @user_turn.cells_empty
+      if @user_turn.playable? == false
+        puts "That column is full, make another selection"
+        until @user_turn.playable? == true
+          @user_turn.go
+          if @user_turn.valid_input? == false
+            puts "Please make a valid selection A - G"
+            until @user_turn.valid_input? == true
+              @user_turn.go
+            end
+          end
+          @user_turn.process_input(@user_turn.column_choice)
+        end
+      end
+      @board.add_x(@user_turn.low_point,@user_turn.column_address[@user_turn.column_choice])
+      @board.render
+      @board.win_scan
+      if @board.win_scan == true
+        break
+      end
+      @board.draw?
+      puts "Player 2 turn"
+      @user_turn.go
+      if @user_turn.valid_input? == false
+        puts "Please make a valid selection A - G"
+        until @user_turn.valid_input? == true
+          @user_turn.go
+        end
+      end
+      @user_turn.process_input(@user_turn.column_choice)
+      @user_turn.cells_empty
+      if @user_turn.playable? == false
+        puts "That column is full, make another selection"
+        until @user_turn.playable? == true
+          @user_turn.go
+          if @user_turn.valid_input? == false
+            puts "Please make a valid selection A - G"
+            until @user_turn.valid_input? == true
+              @user_turn.go
+            end
+          end
+          @user_turn.process_input(@user_turn.column_choice)
+        end
+      end
+      @board.add_o(@user_turn.low_point,@user_turn.column_address[@user_turn.column_choice])
       @board.render
       @board.win_scan
       @board.draw?
